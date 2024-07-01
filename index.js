6898fd51a71c519a793209c9530693c8f5f8932f
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-07-01 00:03:55 -0400

    create root route

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ let persons = [
   },
 ];
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
 app.get("/api/persons", (request, response) => {
   console.log("fetching data from backend...");
   response.json(persons);
